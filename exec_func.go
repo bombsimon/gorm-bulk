9bf070ef4a95ede8a83bf28ed60acdb954eaf62b
Marlon Pina Tojal <dev@fnx.pt>
2019-12-17 18:18:08 +0100

    fix update created_at (#5)
    * fix update created_at
    
    * fix column name
    
    * codestyle

--- exec_func.go
+++ exec_func.go
@@ -47,6 +47,11 @@ func InsertOnDuplicateKeyUpdateFunc(scope *gorm.Scope, columnNames, groups []str
 	var duplicateUpdates []string
 
 	for i := range columnNames {
+		if columnNames[i] == "`created_at`" {
+			continue
+		}
+		
+		// Move on and add columnn...
 		duplicateUpdates = append(
 			duplicateUpdates,
 			fmt.Sprintf("%s = VALUES(%s)", columnNames[i], columnNames[i]),
